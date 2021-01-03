e5717d9d4dd266bd66c24d77443e1c45960b5040
Marion Deveaud <marion.deveaud@siemens.com>
2021-01-03 22:05:21 +0100

    fix(review): add tipps from Kai

--- assets/js/main.js
+++ assets/js/main.js
@@ -249,11 +249,11 @@ initPhotoSwipeFromDOM('.gallery');
 
 // Create map
 var latlng = L.latLng(48.111652, 11.578261);
-var mymap = L.map('mapid').setView(latlng, 19);
+var mymap = L.map('mapid').setView(latlng, 17);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: 17,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
